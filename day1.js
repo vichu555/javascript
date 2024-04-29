0094c60ab6357c4fa139b1967acf12b5e0d9c5b4
vichu555 <80614368+vichu555@users.noreply.github.com>
2024-04-29 10:40:37 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,61 @@
+//variables,data-type,operator
+/*
+//1.add 5 numbers
+let a=3;
+let b=4;
+let c=5;
+let d=7;
+let e=9;
+let res=a+b+c+d+e;
+console.log(res)
+*/
+
+/*
+//2.sum and average of 3 numbers
+let tam=99;
+let eng=97;
+let mat=100;
+let sum=tam+eng+mat;
+let avg=sum/3;
+console.log(`The sum of 3 numbers is ${sum} and their average ${avg.toFixed(2)}`)
+*/
+/*
+//3.swap two numbers with variable
+let a=10;
+let b=11;
+let c;
+console.log(`before swap a : ${a} and b : ${b}`)
+c=b;
+b=a;
+a=c;
+console.log(`after swap a: ${a} and b: ${b}`)
+*/
+
+/*
+//swap two numbers without third variable
+let a=5;
+let b=4;
+console.log(`before swap a : ${a} and b : ${b}`)
+a=a+b;
+b=a-b;
+a=a-b;
+console.log(`after swap a: ${a} and b : ${b}`)
+*/
+/*
+//4.si calculator
+
+let p=10000;
+let r=5;
+let n=2;
+let SI=(p*n*r)/100;
+console.log(`The Simple Interest is ${SI}`)
+*/
+/*
+//5.CI CALCULATOR
+let p=1000;
+let r=5;
+let n=3;
+let CI=p*(Math.pow((1+r/100),n));
+
+console.log(`The compound interest is ${CI}`)
+*/
